ae653a0c78f005c616fa59f7e7535987e362f786
paulaguerrero <aguerrero53@iesnumancia.cat>
2016-01-25 12:17:30 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,9 +12,30 @@ $JSView.controller = {
     mercuri: function(e){
 		$JSView.dataView({},e)
 	},
-    Venus: function(e){
+    venus: function(e){
 		$JSView.dataView({},e)
-        },
+	},
+    terra: function(e){
+		$JSView.dataView({},e)
+	},
+    mart: function(e){
+		$JSView.dataView({},e)
+	},
+    jupiter: function(e){
+		$JSView.dataView({},e)
+	},
+    ura: function(e){
+		$JSView.dataView({},e)
+	},
+    neptu: function(e){
+		$JSView.dataView({},e)
+	},
+    saturn: function(e){
+		$JSView.dataView({},e)
+	},
+    taula: function(e){
+		$JSView.dataView({},e)
+	},
     modalA: function(e){
 		$JSView.dataView({},e)	
 	}
